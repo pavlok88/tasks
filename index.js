ef74cfd4d44149e06e9aef4fe25e2a8d34983c11
pavlok88 <pavlok88@gmail.com>
2021-03-02 11:35:39 +0200

    some, not important changes

--- index.js
+++ index.js
@@ -29,22 +29,22 @@ const forever = require("async/forever");
 
 const random = () => {
     const random = Math.random().toFixed(1) * 10;
-    console.log(random);
     if (!random) {
-      throw new Error("random=0");
+        throw new Error("random=0");
     }
+    console.log(random);
     return random;
   }; // random from 0 to 10
   
   async function foreverNative(targetFunction) {
-    let i = 1;
+    let i = true;
     while (i) {
       (async function f() {
         try {
           const result = await targetFunction();
         } catch (error) {
-          i = 0;
-          console.log(error);
+          i = false;
+          console.log(error.message);
         }
       })();
     }
@@ -53,4 +53,4 @@ const random = () => {
     }, 10 * 1000);
   }
   
-foreverNative(random);
+  foreverNative(random);
